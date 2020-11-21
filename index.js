a32f7c5cb988d24baab050f5d29e12c4a3d04935
rtetelbaum <rtetelbaum@mailbox.org>
2020-11-20 20:57:37 -0500

    lab done

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arrayObjs) {
+	const win = arrayObjs.find(obj => obj.result === "W" )
+	if (win) {
+		return win.year
+	}
+}
